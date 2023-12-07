1b56b3e0063999683583bd1aed15d60d7107c67b
Anton Harbers <Anton.Harbers23@gmail.com>
2023-12-07 11:04:19 +0100

    Merge

--- src/components/todo.js
+++ src/components/todo.js
@@ -1,11 +1,9 @@
 class Todo {
-<<<<<<< HEAD
   constructor(title, description, dueDate, priority, notes) {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
-    this.notes = notes;
   }
 
   updateTodo(title, description, dueDate, priority, notes) {
@@ -13,24 +11,7 @@ class Todo {
     this.description = description;
     this.dueDate = dueDate;
     this.priority = priority;
-    this.notes = notes;
   }
-=======
-    constructor(title, description, dueDate, priority, notes) {
-      this.title = title;
-      this.description = description;
-      this.dueDate = dueDate;
-      this.priority = priority;
-    }
-
-
-    updateTodo(title, description, dueDate, priority, notes){
-        this.title = title;
-        this.description = description;
-        this.dueDate = dueDate;
-        this.priority = priority;
-    }
->>>>>>> 94c324759f049fd2de74ef79c74d54a6a986f4d7
 }
 
 export default Todo;
